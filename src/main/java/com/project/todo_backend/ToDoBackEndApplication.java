999e0baa2b37dc30dfb7dc9cc1cf8fffdc22d4a0
Jimyeong Kim <kjm021221@icloud.com>
2025-03-08 15:24:09 +0900

    🎉Begin a Project

---
+++ src/main/java/com/project/todo_backend/ToDoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project.todo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBackEndApplication.class, args);
+    }
+
+}
